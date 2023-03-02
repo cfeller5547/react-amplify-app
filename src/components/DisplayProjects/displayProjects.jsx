5895a867e1a70c1b54c6c6d1979333b6f5a88d2c
Chris Feller <cfeller5547@sdsu.edu>
2023-03-02 13:04:36 -0800

    updated stick or die page

--- src/components/DisplayProjects/displayProjects.jsx
+++ src/components/DisplayProjects/displayProjects.jsx
@@ -2,28 +2,36 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import {projects} from '../../datapl'
 import "./displayProjects.css"
-import playmediabtn from "../../img/playmediabtn.png";
+import stickordielogo from "../../img/stickordielogo.png";
 import ReactPlayer from 'react-player'
-
+import StickOrDieVideo from "../../video/stickordievideo.mp4"
 
 const DisplayProjects = () => {
     const { id } = useParams();
     const project = projects[id];
     return (
     <div className="project">
+
         <h1>{project.name}</h1>
+        {id === "0" && (
+        <div className="logo-stickordie">
+            <img src={stickordielogo} />
+        </div>
+        )}
         <img src={project.logoImg} />
         <p>
             <b>Description: </b> {project.description}
         </p>
-        
+        <h1>{project.header}</h1>
+        {id === "0" && (
+        <div className="video-stickordie-container">
+            <ReactPlayer url={StickOrDieVideo} controls className="video-stickordie"/>
+        </div>
+        )}
         {id === "1" && (
-        <div>
-            <h1 style={{ textAlign: "center" }}>From Concept to Reality: Our App Journey</h1>
-            <div className="video-container">
-                <ReactPlayer url={project.videoUrl} controls className="video"/>
-                <ReactPlayer url={project.videoUrl2} controls className="video"/>
-            </div>
+        <div className="video-container">
+            <ReactPlayer url={project.videoUrl} controls className="video"/>
+            <ReactPlayer url={project.videoUrl2} controls className="video"/>
         </div>
         )}
     </div>
--- src/datapl.js
+++ src/datapl.js
@@ -6,8 +6,10 @@ export const projects = [
     {    //stickordie pic
         id: 1,
         img: "https://dsm01pap003files.storage.live.com/y4mwUa4NkEbFx6lBlctjCHp5dMkn5WYV1fE9HMCB0rcA-Nfz2fCbo7tLiI2X2TsLgPu3XnHt-bQ_QEF34fo-7APDmvig53h_A18ppmTaKE2Gx1vdH2mKrJTSPosM71o35Y0-G0bfUw7J2oa9T8uxOcOACnkz3DN6sYGnoh_WlgiKK45-VR3FzLGsRG9nr6qs4bM?width=907&height=699&cropmode=none",
-        logoImg: "https://dsm01pap003files.storage.live.com/y4mHZ3ff_YY3ZA3FDKFP9p7h8i1OB5sHqMhf4JyKEBY31wswoaWWIaF1UssamzVtEft4yKqUIGkXEbPtzqGXNJtwtcEb-8UYiW473xPCwVpl44cLODTQBDvTHHhb1_7UNxvEfmt02OhBgqpEaDie7NgBOCtgeAHxpC0MYOr6cvxGSX8PAzw9meB2lIXgrsyXFQW?width=1274&height=700&cropmode=none",
-        description: "Stick or Die is a native android mobile game developed which has resulted in a refinement of my skills in OOP. Some technologies utilized include Java, Google Play Games APIs to create a global leaderboard, XML Layout files, various data structures"
+        logoImg: "https://drive.google.com/file/d/1e6F14gEDWdT-9YlxnA8_WtGusuVE5v4P/view?usp=share_link",
+        description: "Stick or Die is a native android mobile game developed which has resulted in a refinement of my skills in OOP. Some technologies utilized include Java, Google Play Games APIs to create a global leaderboard, XML Layout files, various data structures",
+        videoUrl: "https://www.dropbox.com/s/gv5ycy5m9enb3aj/stickordievideo.mp4?dl=0",
+        header: "Stick Or Die - A Demo Video",
     },
     {   //diamondrun pic
         id: 2,
@@ -16,10 +18,12 @@ export const projects = [
         description: "Diamond Run is a native android mobile game currently being built using Android Studio with multiplayer incorporated into it. Some technologies utilized include Java, a Firebase cloud-hosted NoSQL Realtime Database, XML Layout files, various data structures",
         videoUrl: "https://www.dropbox.com/s/bgs6btvet0dhg19/diamondrunmultiplayer.mp4?dl=0",
         videoUrl2: "https://www.dropbox.com/s/bsz9iivh5ojl2ng/Diamond%20Run%20Ad.mp4?dl=0",
+        header: "From Concept to Reality: Our App Journey",
     },
     {   //ai pic
         id: 3,
         img: "https://dsm01pap003files.storage.live.com/y4mR7NMVw7DTiHpHjW3emB3wbiehbInvPvblW2eq7-y7GJAmPfDglJHGPJf-IHs9bXHBAd1KrEsM5aLGYNt22Yf9SuVJ41yRt3_GQ8CzT2u2XTKeU0I2y_KrvV8Yz1EDN0WkxWWklYAG-vXI0Kw-zdNEvDeIj3kLphSW6ILhg5478vXwV9FNhsN-YgGH8na4VXT?width=909&height=700&cropmode=none",
-        description: "Working with a team following the Agile framework Scrum for a Semantic Primes project isolated on a virtual machine. Involving a database, a Machine Learning model, Word2Vec Algorithm, KNN Algorithm. I have been assigned the role of creating the model." 
+        description: "Working with a team following the Agile framework Scrum for a Semantic Primes project isolated on a virtual machine. Involving a database, a Machine Learning model, Word2Vec Algorithm, KNN Algorithm. I have been assigned the role of creating the model.",
+        header: "This is my AI Header",
     },
 ];
